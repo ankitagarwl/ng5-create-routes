85e8b32d8cbb583f494c3fefa797291ea92d4387
Ankit Agarwal <ankitagarwl@hotmail.com>
2018-04-16 16:18:56 +0530

    first commit

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class UserListComponent implements OnInit {
+
+users = [
+  { name : 'john' },
+  { name : 'peter'},
+  { name : 'judy' }
+  
+];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
